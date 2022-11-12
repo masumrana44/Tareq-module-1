163b30b16bf2e41ae191841547d406973e479599
Masum-Rana <masumrana446267@gmail.com>
2022-11-12 23:04:37 +0600

    tareq module-1

---
+++ variable.js
@@ -0,0 +1,106 @@
+// 1.What is variable in js ?
+
+// Rules:-variable er nam sob somoi camel case likhte history.
+// Rules:-variable nam sob somoi meaningful thakte hobe 
+var number = 38;
+var tareqHossainAge = 22;
+
+var schoolName = 'haidargonj model school';
+
+// Javascript variable type
+var fevoNumber = 12;
+var myName = 'Tareq Hossain';
+var isMarred = false;
+
+var myBioData = null;
+var myGfName = undefined;
+
+//  javascript addition (+)
+var num1 = 2;
+var num2 = 5;
+var total = num1 + num2;
+console.log(total)
+var totalMoney = 40 + 20;
+console.log(totalMoney);
+
+// javascript substruction (-)
+var num3 = 35;
+var num4 = 10;
+var totalSubstruction = num3 - num4;
+console.log(totalSubstruction);
+
+// javascript Multiplication (*)
+var num5 = 3;
+var num6 = 4;
+var totalMultiplication = num5 * num6;
+console.log(totalMultiplication);
+
+// Javascript Division (/)
+var num7 = 15;
+var num8 = 4;
+var totalDivision = num7 / num8;
+console.log(totalDivision);
+
+// Javascript remainder (%)
+var num9 = 15;
+var num10 = 2;
+var mudulas = num9 % num10;
+console.log(mudulas);
+
+
+// 2.Javascript data type
+//  3.What difference of betweeen null and undefined
+
+
+
+// How to cheak javascript data type 
+// typeof method 
+var myGfName = 'Nargish';
+var typeData = typeof (myGfName);
+console.log(typeData)
+
+var myGfAge = 23;
+console.log(typeof (myGfAge))
+
+var isMarried = false;
+console.log(typeof (isMarred))
+
+var myFakeAge = '25';
+console.log(typeof(myFakeAge))
+
+
+// convert string to number
+
+// parseInt method 
+var stringToNumber = parseInt(myFakeAge);
+console.log(stringToNumber)
+console.log(typeof (stringToNumber));
+
+// parseFloat method 
+var sum = '36';
+var stringToNumber2 = parseFloat(sum);
+console.log(stringToNumber2, typeof (stringToNumber2));
+
+// parseInt
+var flotNumber = '55.6';
+console.log(parseInt(flotNumber), typeof (flotNumber));
+ 
+// parseFloat
+var flotNumber2 = '55.6';
+console.log(parseFloat(flotNumber2), typeof (flotNumber2));
+ 
+
+const sum2 = 22.6666666666;
+
+var toFiexedFlotNumber = sum2.toFixed(2);
+
+
+
+console.log(toFiexedFlotNumber, typeof (toFiexedFlotNumber));
+
+var toFiexedFlotNumber2 = parseFloat(toFiexedFlotNumber);
+console.log(toFiexedFlotNumber2, typeof (toFiexedFlotNumber2));
+
+
+ 
+
